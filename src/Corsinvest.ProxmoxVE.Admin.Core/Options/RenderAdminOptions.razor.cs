f57bc784337d28518b1752203496f010cfba82f4
daniele.corsini@corsinvest.it <daniele.corsini@corsinvest.it>
2023-06-20 17:18:58 +0200

    Option save

--- src/Corsinvest.ProxmoxVE.Admin.Core/Options/RenderAdminOptions.razor.cs
+++ src/Corsinvest.ProxmoxVE.Admin.Core/Options/RenderAdminOptions.razor.cs
@@ -2,6 +2,8 @@
  * SPDX-FileCopyrightText: Copyright Corsinvest Srl
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+using Corsinvest.AppHero.Core.UI;
+
 namespace Corsinvest.ProxmoxVE.Admin.Core.Options;
 
 public partial class RenderAdminOptions
@@ -12,16 +14,23 @@ public partial class RenderAdminOptions
     {
         foreach (var item in Options.Clusters)
         {
-            var client = await PveClientService.GetClient(item);
-            if (client != null)
+            try
             {
-                //if (!await PveAdminHelper.CheckIsValidVersion(client))
-                //{
-                //    UINotifier.Show(L["{0} - Proxmoxm VE version nont valid! Required {1}", item.FullName, PveAdminHelper.MinimalVersion], UINotifierSeverity.Error);
-                //}
-                var info = await client.GetClusterInfo();
-                item.Name = info.Name;
-                item.Type = info.Type;
+                var client = await PveClientService.GetClient(item);
+                if (client != null)
+                {
+                    //if (!await PveAdminHelper.CheckIsValidVersion(client))
+                    //{
+                    //    UINotifier.Show(L["{0} - Proxmoxm VE version nont valid! Required {1}", item.FullName, PveAdminHelper.MinimalVersion], UINotifierSeverity.Error);
+                    //}
+                    var info = await client.GetClusterInfo();
+                    item.Name = info.Name;
+                    item.Type = info.Type;
+                }
+            }
+            catch (Exception ex)
+            {
+                UINotifier.Show(ex.Message, UINotifierSeverity.Error);
             }
         }
 
